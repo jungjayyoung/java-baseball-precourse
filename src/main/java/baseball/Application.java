f3b8b1bbfaee18b5e83cc06c4350391a3f3d5b60
jungjayyoung <jaywjd@gmail.com>
2022-02-27 20:08:45 +0900

    feat: 게임 시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import domain.Player;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+
+        Player player = new Player();
+        player.gameStart();
+
     }
 }
